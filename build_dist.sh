fea23976188bf508d1b65c016e13ff215f8d8ddb
befulton <befulton@iu.edu>
2022-11-18 16:46:10 -0500

    Add manual to release builds

--- build_dist.sh
+++ build_dist.sh
@@ -1,3 +1,3 @@
 mkdir distro
 
-tar --transform "s,^,CAGEE/," --owner=0 --group=0 -czf distro/CAGEE0.1.0.0.tar.gz src test.cpp main.cpp diffmat_precalc.cpp CHANGELOG.md LICENSE CMakeLists.txt README.md config.h.in examples LBFGSpp/
+tar --transform "s,^,CAGEE/," --owner=0 --group=0 -czf distro/CAGEE0.1.0.0.tar.gz src test.cpp main.cpp diffmat_precalc.cpp CHANGELOG.md LICENSE CMakeLists.txt README.md config.h.in examples LBFGSpp/ docs/manual
